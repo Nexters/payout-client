490a698ff2a32fbdee1d8e97239eec6fcda1cd6c
정진리/스토어프론트팀 <yeti.jeong@musinsa.com>
2024-02-14 13:09:23 +0900

    feat: implement ticker ui styling

--- src/app/ticker/_components/ticker-list.tsx
+++ src/app/ticker/_components/ticker-list.tsx
@@ -1,38 +1,24 @@
 "use client";
 
-import Toast from "@/components/common/toast/toast";
-import { Button } from "@/components/ui/button";
 import React from "react";
-import { toast } from "sonner";
+import TickerItem from "./ticker-item";
 
 const TickerList = React.memo(() => {
-  const onTickerDelete = React.useCallback(() => {
-    toast.custom((t) => <Toast t={t} title={`You deleted ${"GOOGLE"}`} isRevertable={true} />);
-  }, []);
 
   return (
     <div className="flex h-full w-full flex-1 flex-col items-start">
-      <div className="flex w-full flex-1 flex-col items-start gap-4">
-        <div className="flex w-full items-center justify-between ">
-          <p className="text-2xl font-bold">GOOGLE</p>
-          <Button onClick={onTickerDelete}>Delete</Button>
-        </div>
-        <div className="flex w-full items-center justify-between ">
-          <p className="text-2xl font-bold">GOOGLE</p>
-          <Button onClick={onTickerDelete}>Delete</Button>
-        </div>
-        <div className="flex w-full items-center justify-between ">
-          <p className="text-2xl font-bold">GOOGLE</p>
-          <Button onClick={onTickerDelete}>Delete</Button>
-        </div>
-        <div className="flex w-full items-center justify-between ">
-          <p className="text-2xl font-bold">GOOGLE</p>
-          <Button onClick={onTickerDelete}>Delete</Button>
-        </div>
-        <div className="flex w-full items-center justify-between ">
-          <p className="text-2xl font-bold">GOOGLE</p>
-          <Button onClick={onTickerDelete}>Delete</Button>
-        </div>
+      <div className="flex w-full flex-1 flex-col items-start gap-4 overflow-scroll px-4 py-6">
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
+        <TickerItem />
       </div>
     </div>
   );
--- src/app/ticker/page.tsx
+++ src/app/ticker/page.tsx
@@ -1,28 +1,27 @@
-import { Plus } from "lucide-react";
-import { Drawer as DrawerPrimitive, DrawerTrigger } from "@/components/ui/drawer";
+import { Drawer as DrawerPrimitive } from "@/components/ui/drawer";
 import { TickerDrawer } from "./_components/ticker-drawer";
+import Intro from "./_components/intro";
 import TickerList from "./_components/ticker-list";
 
-const TickerPage = () => {
+export default function TickerPage() {
   return (
     <DrawerPrimitive>
-      <div className="mx-auto flex h-full w-full max-w-[--max-width] flex-col py-6 text-2xl font-bold">
-        <p>You Added 0 Tickers</p>
-        <div className="flex flex-col justify-start py-10 text-lg">
-          <DrawerTrigger asChild>
-            <div className="flex w-full cursor-pointer items-center justify-start gap-4">
-              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 text-[#8B95A1]">
-                <Plus />
-              </div>
-              <p>Add Ticker</p>
-            </div>
-          </DrawerTrigger>
-        </div>
+      <div className="flex h-full w-full flex-col pt-11">
+        <Intro />
+        {/* TODO Ticker state */}
         <TickerList />
+
       </div>
       <TickerDrawer />
+      <div className="fixed inset-x-0 bottom-4 flex w-full justify-center pt-12" 
+        style={{
+          backgroundImage: "linear-gradient(0deg, var(--color-white) 52.78%, rgba(255, 255, 255, 0.00) 100%)",
+        }}>
+        <button className="mx-5 w-full rounded-lg bg-main-700 p-4">
+          <p className="text-h5 text-white">Finish</p>
+        </button>
+      </div>
     </DrawerPrimitive>
   );
 };
 
-export default TickerPage;
