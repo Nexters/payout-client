52d83179085542ce1d7461d458be1479eefd9b5a
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-14 23:51:18 +0900

    feat: implement footer

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -26,11 +26,6 @@ export default function Home() {
           Check My Dividend Portfolio
         </Button>
       </div>
-      <div className="fixed bottom-0 left-1/2 w-full max-w-[768px] -translate-x-1/2 bg-[linear-gradient(0deg,_#FFF_52.78%,_rgba(255,_255,_255,_0.00)_100%);] p-5 pt-[50px]">
-        <Button variant={"primary"} size={"large"} className="w-full ">
-          Check My Dividend Portfolio
-        </Button>
-      </div>
     </>
   );
 }
