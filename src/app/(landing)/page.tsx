eefa9f53c5f36ca1fae90019c10d20044088e3f0
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-14 23:46:29 +0900

    feat: implement bottom CTA with global button component

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,7 +6,7 @@ import TargetUser from "./_components/target-user";
 import YearlyDividend from "./_components/yearly-dividend";
 import FAQ from "./_components/faq";
 import Footer from "./_components/footer";
-import { Button } from "@/components/button/button";
+import { Button } from "@/components/Button/Button";
 
 export default function Home() {
   return (
@@ -20,6 +20,11 @@ export default function Home() {
         <YearlyDividend />
         <FAQ />
         <Footer />
+      </div>
+      <div className="fixed bottom-0 left-1/2 w-full max-w-[768px] -translate-x-1/2 bg-[linear-gradient(0deg,_#FFF_52.78%,_rgba(255,_255,_255,_0.00)_100%);] p-5 pt-[50px]">
+        <Button variant={"primary"} size={"large"} className="w-full ">
+          Check My Dividend Portfolio
+        </Button>
       </div>
       <div className="fixed bottom-0 left-1/2 w-full max-w-[768px] -translate-x-1/2 bg-[linear-gradient(0deg,_#FFF_52.78%,_rgba(255,_255,_255,_0.00)_100%);] p-5 pt-[50px]">
         <Button variant={"primary"} size={"large"} className="w-full ">
