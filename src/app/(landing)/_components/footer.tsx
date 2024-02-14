6f598677dd43fb3bcf23bcaa0deed9e0ce841baf
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-14 23:51:18 +0900

    feat: implement footer

---
+++ src/app/(landing)/_components/footer.tsx
@@ -0,0 +1,16 @@
+import Logo from "@/components/logo/logo";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="flex w-full flex-col items-start justify-center gap-5 bg-grey-50 px-5 py-8">
+      <Logo />
+      <p className="text-left text-body3 text-grey-600">
+        The investment information provided by Payout is solely for reference purposes and is not intended for any kind
+        of financial advices. Stock Data are updated daily at midnight UTC, and Dividend Data are estimated based on the
+        data of the preceding year.
+      </p>
+      <p className="text-body4 text-grey-500">© 2024 Payout. All Rights Reserved.</p>
+    </div>
+  );
+};
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,6 +5,7 @@ import MonthlyDividend from "./_components/monthly-dividend";
 import SectorBalanced from "./_components/sector-balanced";
 import TargetUser from "./_components/target-user";
 import YearlyDividend from "./_components/yearly-dividend";
+import { Footer } from "./_components/footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <SectorBalanced />
         <MonthlyDividend />
         <YearlyDividend />
+        <Footer />
       </div>
       <div className="fixed bottom-0 left-1/2 w-full max-w-[768px] -translate-x-1/2 bg-[linear-gradient(0deg,_#FFF_52.78%,_rgba(255,_255,_255,_0.00)_100%);] p-5 pt-[50px]">
         <Button variant={"primary"} size={"large"} className="w-full ">
