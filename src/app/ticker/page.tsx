10ccde85f0c0c2925b16809ba258ec4950426716
JinleeJeong <pjjr0118@gmail.com>
2024-02-16 18:08:37 +0900

    style: fixed to sticky

--- src/app/ticker/page.tsx
+++ src/app/ticker/page.tsx
@@ -11,15 +11,16 @@ export default function TickerPage() {
         <Content />
       </div>
       <TickerDrawer />
-      <div className="fixed inset-x-0 bottom-4 flex w-full justify-center pt-12" 
+      <div
+        className="sticky inset-x-0 bottom-4 flex w-full justify-center pt-12"
         style={{
           backgroundImage: "linear-gradient(0deg, var(--color-white) 52.78%, rgba(255, 255, 255, 0.00) 100%)",
-        }}>
+        }}
+      >
         <button className="mx-5 w-full rounded-lg bg-main-700 p-4">
           <p className="text-h5 text-white">Finish</p>
         </button>
       </div>
     </DrawerPrimitive>
   );
-};
-
+}
