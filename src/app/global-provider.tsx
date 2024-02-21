db8b80286b96e817dedd4f9dc728da42aa9197d7
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-21 20:53:29 +0900

    feat: implement api gen with swagger

--- src/app/global-provider.tsx
+++ src/app/global-provider.tsx
@@ -2,7 +2,11 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import axios from "axios";
 import React from "react";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_HOST;
+console.log(process.env.NEXT_PUBLIC_API_HOST);
 
 export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   const [client] = React.useState(() => new QueryClient());
