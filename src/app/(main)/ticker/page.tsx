70e7f615b28cc058159d0c04b42fbdfbf74723f4
정진리/스토어프론트팀 <yeti.jeong@musinsa.com>
2024-02-23 00:17:34 +0900

    style: remove margin TickerPage

--- src/app/(main)/ticker/page.tsx
+++ src/app/(main)/ticker/page.tsx
@@ -4,9 +4,9 @@ import TickerFooter from "./_components/ticker-footer";
 
 export default function TickerPage() {
   return (
-    <div className="mt-10">
+    <>
       <TickerContent />
       <TickerFooter />
-    </div>
+    </>
   );
 }
