a4cbf51330911a1ebc192b6ceca406ccbd3e2447
정진리/스토어프론트팀 <yeti.jeong@musinsa.com>
2024-02-25 02:14:50 +0900

    feat: apply a tag on header

--- src/components/common/navigation-bar/navigation-bar.tsx
+++ src/components/common/navigation-bar/navigation-bar.tsx
@@ -3,9 +3,9 @@ import Logo from "../logo/logo";
 
 const NavigationBar = () => {
   return (
-    <div className="flex w-full items-center justify-start bg-transparent px-5 py-2.5">
+    <a href="/" className="flex w-full items-center justify-start bg-transparent px-5 py-2.5">
       <Logo />
-    </div>
+    </a>
   );
 };
 
