2bf097de29fce12cb87dea34f16f0a08f752b22b
정진리/스토어프론트팀 <yeti.jeong@musinsa.com>
2024-02-24 20:42:42 +0900

    feat: remove navigationBar on landingLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { GlobalProvider } from "./global-provider";
-import NavigationBar from "@/components/common/navigation-bar/navigation-bar";
 import { Toaster } from "@/components/ui/sonner";
 
 const dm_sans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
@@ -19,7 +18,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={dm_sans.className}>
         <GlobalProvider>
           <div className="relative flex h-full w-full flex-col items-center justify-center">
-            <NavigationBar />
             <div className="h-full w-full flex-1 overflow-auto">{children}</div>
             <Toaster position="bottom-center" />
           </div>
