8a89be6ea6aaa6c86140fb448e86e7a705fa848d
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-27 22:58:43 +0900

    feat: integrate bubble chart with backend data

--- src/app/(main)/report/dividend/yearly/page.tsx
+++ src/app/(main)/report/dividend/yearly/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Header } from "./_components/header";
 import { DividendList } from "./_components/dividend-list";
 import { Dividend } from "../_components/dividend-row";
+import { useYearlyDividendMutation } from "@/state/queries/use-yearly-dividend-mutation";
+import { YearlyDividendResponse } from "@/api/generated/endpoint.schemas";
 
 const dummyDividendList: Dividend[] = [
   {
@@ -337,6 +339,22 @@ const dummyDividendList: Dividend[] = [
 ];
 
 const YearlyDividendPage = React.memo(() => {
+  const { mutate: mutateYearlyDividend, data: yearlyDividendData } = useYearlyDividendMutation();
+
+  React.useEffect(() => {
+    mutateYearlyDividend();
+  }, [mutateYearlyDividend]);
+
+  const yearlyDividends: YearlyDividendResponse = React.useMemo(() => {
+    return {
+      totalDividend: yearlyDividendData?.totalDividend ?? 0,
+      dividends:
+        yearlyDividendData?.dividends.sort((a, b) => {
+          return b.totalDividend - a.totalDividend;
+        }) ?? [],
+    };
+  }, [yearlyDividendData]);
+
   return (
     <div className="flex size-full flex-col items-center justify-start gap-5">
       <Header />
---
+++ src/state/queries/use-yearly-dividend-mutation.ts
@@ -0,0 +1,33 @@
+import { getYearlyDividends } from "@/api/generated/endpoint";
+import { YearlyDividendResponse } from "@/api/generated/endpoint.schemas";
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useStocksStore } from "../stores/stocks-store";
+import React from "react";
+
+export const yearlyDividendQueryKeys = createQueryKeys("yearly-dividend");
+
+export const useYearlyDividendMutation = ()=> {
+  const { stocks } = useStocksStore();
+
+  const tickerShares = React.useMemo(() => stocks.map((stock) => ({
+    share: stock.count,
+    ticker: stock.ticker ?? "",
+  })), [stocks])
+
+  const requestClient = async (): Promise<YearlyDividendResponse> => {
+    const response = await getYearlyDividends({
+      tickerShares
+    });
+    return response.data;
+  };
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: yearlyDividendQueryKeys._def,
+    mutationFn: requestClient,
+    onSuccess: (data) => {
+      queryClient.setQueryData(yearlyDividendQueryKeys._def, data);
+    },
+  });
+};
