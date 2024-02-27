b80977a3be7854f89b0e718dd84653f3ecad7074
Bokdol11859 <2019147551@yonsei.ac.kr>
2024-02-27 22:58:43 +0900

    feat: integrate bubble chart with backend data

--- src/app/(main)/report/page.tsx
+++ src/app/(main)/report/page.tsx
@@ -6,21 +6,37 @@ import { MonthlyDividend } from "./_components/monthly-dividend";
 import { useStocksSectorRatioMutation } from "@/state/queries/use-stocks-sector-ratio";
 import { AnnualDividend } from "./_components/annual-dividend";
 import { Loader2Icon } from "lucide-react";
+import { useYearlyDividendMutation } from "@/state/queries/use-yearly-dividend-mutation";
+import { YearlyDividendResponse } from "@/api/generated/endpoint.schemas";
 
 const ReportPage = () => {
-  const { mutate, data } = useStocksSectorRatioMutation();
+  const { mutate: mutateStocksSectorRatio, data: stocksSectorRatioData } = useStocksSectorRatioMutation();
+  const { mutate: mutateYearlyDividend, data: yearlyDividendData } = useYearlyDividendMutation();
 
   React.useEffect(() => {
-    mutate();
-  }, [mutate])
+    mutateStocksSectorRatio();
+    mutateYearlyDividend();
+  }, [mutateStocksSectorRatio, mutateYearlyDividend]);
 
-  const sectors = React.useMemo(() => {
-    return data?.sort((a, b) => {
-      return b.sectorRatio - a.sectorRatio;
-    }) ?? []
-  }, [data])
+  const stocksSectorRatio = React.useMemo(() => {
+    return (
+      stocksSectorRatioData?.sort((a, b) => {
+        return b.sectorRatio - a.sectorRatio;
+      }) ?? []
+    );
+  }, [stocksSectorRatioData]);
 
-  if (!data) {
+  const yearlyDividends: YearlyDividendResponse = React.useMemo(() => {
+    return {
+      totalDividend: yearlyDividendData?.totalDividend ?? 0,
+      dividends:
+        yearlyDividendData?.dividends.sort((a, b) => {
+          return b.totalDividend - a.totalDividend;
+        }) ?? [],
+    };
+  }, [yearlyDividendData]);
+
+  if (!stocksSectorRatioData || !yearlyDividendData) {
     return (
       <div className="flex size-full items-center justify-center">
         <Loader2Icon className="animate-spin" />
@@ -31,11 +47,11 @@ const ReportPage = () => {
   return (
     <div className="size-full">
       <div className="flex size-full flex-col">
-        <SectorFocus data={sectors} />
+        <SectorFocus data={stocksSectorRatio} />
         <Divider />
         <MonthlyDividend />
         <Divider />
-        <AnnualDividend />
+        <AnnualDividend data={yearlyDividends} />
       </div>
     </div>
   );
