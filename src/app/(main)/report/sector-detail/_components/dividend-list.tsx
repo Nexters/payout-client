008750533bf03873998c0f7469dbebff4e2c4248
정진리/스토어프론트팀 <yeti.jeong@musinsa.com>
2024-02-28 11:50:48 +0900

    feat: connect api to ui on sector insight

--- src/app/(main)/report/sector-detail/_components/dividend-list.tsx
+++ src/app/(main)/report/sector-detail/_components/dividend-list.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { StockShareResponse } from "@/api/generated/endpoint.schemas";
+import { DividendRow } from "../../dividend/_components/dividend-row";
 
 export const DividendList = React.memo(({ dividendList }: { dividendList: StockShareResponse[] }) => {
   return (
     <div className="flex w-full flex-col items-center justify-start gap-6 overflow-y-auto px-5 py-8">
       {dividendList.map((dividend, idx) => (
-        <></>
-        // <DividendRow dividend={dividend} key={idx} />
+        <DividendRow
+          key={idx}
+          dividend={{
+            ...dividend.stockResponse,
+            share: dividend.share,
+          }}
+        />
       ))}
     </div>
   );
