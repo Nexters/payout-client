c5227c8df4bc5a64ea2bacc6f509e3552d5bbae5
정진리/스토어프론트팀 <yeti.jeong@musinsa.com>
2024-03-02 00:06:50 +0900

    style: toast pc ui width 100%

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -11,7 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group [&>li]:w-full"
       toastOptions={{
         classNames: {
           toast:
